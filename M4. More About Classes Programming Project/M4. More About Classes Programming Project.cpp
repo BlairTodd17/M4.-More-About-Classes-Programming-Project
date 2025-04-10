ce32cfd75d88333c162087c318b066635c6f704d
Blair Todd <bjbbt4@outlook.com>
2025-04-10 12:39:59 -0400

    boom

--- M4. More About Classes Programming Project/M4. More About Classes Programming Project.cpp
+++ M4. More About Classes Programming Project/M4. More About Classes Programming Project.cpp
@@ -1,150 +1,103 @@
 #include <iostream>
 #include <string>
-#include <sstream>
-#include <cmath>
-#include <algorithm>
 using namespace std;
 
-class Date {
+class ParkedCar {
 private:
-    int day, month, year;
-
-    static const int daysInMonth[12];
-    static const string monthNames[12];
-
-    bool isLeapYear(int y) const {
-        return (y % 4 == 0 && y % 100 != 0) || (y % 400 == 0);
-    }
-
-    int getDaysInMonth(int m, int y) const {
-        if (m == 2) return isLeapYear(y) ? 29 : 28;
-        return daysInMonth[m - 1];
-    }
-
-    bool isValidDate(int d, int m, int y) const {
-        return (y > 0 && m >= 1 && m <= 12 && d >= 1 && d <= getDaysInMonth(m, y));
-    }
+    string make, model, color, licenseNumber;
+    int minutesParked;
 
 public:
-    Date(int d = 1, int m = 1, int y = 2000) {
-        if (isValidDate(d, m, y)) {
-            day = d; month = m; year = y;
+    ParkedCar(string mk = "", string mdl = "", string clr = "", string lic = "", int mins = 0)
+        : make(mk), model(mdl), color(clr), licenseNumber(lic), minutesParked(mins) {}
+
+    int getMinutesParked() const { return minutesParked; }
+    string getMake() const { return make; }
+    string getModel() const { return model; }
+    string getColor() const { return color; }
+    string getLicenseNumber() const { return licenseNumber; }
+};
+
+class ParkingMeter {
+private:
+    int minutesPurchased;
+
+public:
+    ParkingMeter(int mins = 0) : minutesPurchased(mins) {}
+
+    int getMinutesPurchased() const { return minutesPurchased; }
+};
+
+class ParkingTicket {
+private:
+    string officerName, badgeNumber;
+    string carMake, carModel, carColor, licenseNumber;
+    int fine;
+    int minutesOver;
+
+public:
+    ParkingTicket(const ParkedCar& car, int overMinutes, const string& name, const string& badge) {
+        carMake = car.getMake();
+        carModel = car.getModel();
+        carColor = car.getColor();
+        licenseNumber = car.getLicenseNumber();
+        minutesOver = overMinutes;
+        officerName = name;
+        badgeNumber = badge;
+
+        fine = 25;
+        if (overMinutes > 60) {
+            int additionalHours = (overMinutes - 1) / 60;
+            fine += additionalHours * 10;
         }
-        else {
-            day = 1; month = 1; year = 2000;
-        }
     }
 
-    void setDate(int d, int m, int y) {
-        if (isValidDate(d, m, y)) {
-            day = d; month = m; year = y;
-        }
-        else {
-            cout << "Invalid date: " << m << "/" << d << "/" << y << endl;
-        }
-    }
-
-    Date& operator++() {
-        day++;
-        if (day > getDaysInMonth(month, year)) {
-            day = 1;
-            month++;
-            if (month > 12) {
-                month = 1;
-                year++;
-            }
-        }
-        return *this;
-    }
-
-    Date operator++(int) {
-        Date temp = *this;
-        ++(*this);
-        return temp;
-    }
-
-    Date& operator--() {
-        day--;
-        if (day == 0) {
-            month--;
-            if (month == 0) {
-                month = 12;
-                year--;
-            }
-            day = getDaysInMonth(month, year);
-        }
-        return *this;
-    }
-
-    Date operator--(int) {
-        Date temp = *this;
-        --(*this);
-        return temp;
-    }
-
-    int operator-(const Date& other) const {
-        return abs(toDays() - other.toDays());
-    }
-
-    int toDays() const {
-        int totalDays = day;
-        for (int i = 1; i < year; ++i)
-            totalDays += isLeapYear(i) ? 366 : 365;
-        for (int i = 1; i < month; ++i)
-            totalDays += getDaysInMonth(i, year);
-        return totalDays;
-    }
-
-    friend ostream& operator<<(ostream& out, const Date& d) {
-        out << monthNames[d.month - 1] << " " << d.day << ", " << d.year;
-        return out;
-    }
-
-    friend istream& operator>>(istream& in, Date& d) {
-        string input;
-        cout << "Enter date (MM/DD/YYYY): ";
-        getline(in, input);
-        replace(input.begin(), input.end(), '/', ' ');
-        stringstream ss(input);
-        int m, day, y;
-        ss >> m >> day >> y;
-        if (d.isValidDate(day, m, y)) {
-            d.day = day; d.month = m; d.year = y;
-        }
-        else {
-            cout << "Invalid date entered.\n";
-        }
-        return in;
+    void printTicket() const {
+        cout << "\n*** Parking Ticket ***\n";
+        cout << "Officer: " << officerName << "  Badge: " << badgeNumber << "\n";
+        cout << "Car - License: " << licenseNumber << ", Make: " << carMake << ", Model: " << carModel << ", Color: " << carColor << "\n";
+        cout << "Minutes Over: " << minutesOver << "\n";
+        cout << "Fine: $" << fine << "\n";
     }
 };
 
-const int Date::daysInMonth[12] = { 31,28,31,30,31,30,31,31,30,31,30,31 };
-const string Date::monthNames[12] = {
-    "January","February","March","April","May","June",
-    "July","August","September","October","November","December"
+class PoliceOfficer {
+private:
+    string name;
+    string badgeNumber;
+
+public:
+    PoliceOfficer(string n = "", string b = "") : name(n), badgeNumber(b) {}
+
+    void inspectCar(const ParkedCar& car, const ParkingMeter& meter) {
+        int parked = car.getMinutesParked();
+        int purchased = meter.getMinutesPurchased();
+        if (parked > purchased) {
+            int minutesOver = parked - purchased;
+            ParkingTicket ticket(car, minutesOver, name, badgeNumber);
+            ticket.printTicket();
+        }
+        else {
+            cout << "\nNo violation. Car is legally parked.\n";
+        }
+    }
 };
 
 int main() {
-    Date d1(4, 10, 2014), d2(4, 18, 2014);
-    cout << "Date 1: " << d1 << "\n";
-    cout << "Date 2: " << d2 << "\n";
-    cout << "Days between: " << (d2 - d1) << " days\n\n";
+    ParkedCar car1("Toyota", "Camry", "Red", "XYZ123", 30);
+    ParkingMeter meter1(40);
+    PoliceOfficer officer1("John Doe", "5678");
+    officer1.inspectCar(car1, meter1);
 
-    Date leap(2, 29, 2008);
-    cout << "Original leap day: " << leap << "\n";
-    cout << "After pre-decrement: " << --leap << "\n";
-    cout << "After pre-increment: " << ++leap << "\n";
+    ParkedCar car2("Honda", "Accord", "Blue", "ABC987", 70);
+    ParkingMeter meter2(60);
+    PoliceOfficer officer2("Jane Smith", "1234");
+    officer2.inspectCar(car2, meter2);
 
-    Date dec31(12, 31, 2024);
-    cout << "\nOriginal date: " << dec31 << "\n";
-    cout << "After post-increment: " << dec31++ << "\n";
-    cout << "Now: " << dec31 << "\n";
-    cout << "After post-decrement: " << dec31-- << "\n";
-    cout << "Now: " << dec31 << "\n";
-
-    Date inputDate;
-    cin >> inputDate;
-    cout << "You entered: " << inputDate << "\n";
+    ParkedCar car3("Ford", "Mustang", "Black", "LMN456", 190);
+    ParkingMeter meter3(60);
+    PoliceOfficer officer3("James Brown", "4321");
+    officer3.inspectCar(car3, meter3);
 
     return 0;
 }
